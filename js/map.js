2d89d3b1b6d1938156af91af905d047dd7bfdf93
francesco-ftk <francesco.fantechi3@stud.unifi.it>
2021-06-17 08:15:35 +0200

    Interfaccia #13

--- js/map.js
+++ js/map.js
@@ -1,10 +1,10 @@
 if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
+    navigator.geolocation.getCurrentPosition(setMap);
 } else {
     x.innerHTML = "Geolocation is not supported by this browser.";
 }
 
-function showPosition(position) {
+function setMap(position) {
     var mymap = L.map('mapid', { zoomControl:true }).setView([position.coords.latitude, position.coords.longitude], 16);
 
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -16,13 +16,6 @@ function showPosition(position) {
         zoomOffset: -1,
         accessToken: 'pk.eyJ1IjoiYmFuem8iLCJhIjoiY2twODZkZXFjMDV5ODJ5b2dtc3lyYm5qMyJ9.c-pRfXAUsbjdQJ7FpUjZuQ'
     }).addTo(mymap);
-
-    /*var circleMarker = L.circleMarker([position.coords.latitude, position.coords.longitude], {
-        color: 'blu',
-        fillColor: '#000dff',
-        fillOpacity: 1,
-        radius: 10
-    }).addTo(mymap);*/
 
     var myIcon = L.icon({
         iconUrl: 'Immagini/marker.png',
@@ -74,7 +67,9 @@ function showPosition(position) {
         .catch(function(error){console.log(error);});
 }
 
-/*var mymap = L.map('mapid');//.setView([43.773, 11.255], 16);
+/*
+
+var mymap = L.map('mapid');//.setView([43.773, 11.255], 16);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -115,4 +110,5 @@ var markers = L.markerClusterGroup();
 markers.addLayer(L.marker([43.773, 11.258])); <!-- Cordinate da passare con pagine wikipedia -->
 markers.addLayer(L.marker([43.773, 11.257]).bindPopup("<img src='http://www.lib.utexas.edu/maps/historical/newark_nj_1922.jpg' width='200px'>").openPopup());
 mymap.addLayer(markers);
+
 */
