b1f11a17d43491fb95ccdfa661ed9bc28a964f85
FrancescoBettazzi <francescobettazzi99@gmail.com>
2021-06-22 17:51:55 +0200

    canvas #13

--- js/myCamera.js
+++ js/myCamera.js
@@ -16,8 +16,7 @@ function openCamera(){
     canvasElement.width = webcamElement.width;
     canvasElement.height = webcamElement.height; //width*.75;
 
-    /*if(window.innerWidth>window.innerHeight) {
-        if(window.innerHeight)
+    /*if(window.innerHeight ) {
     }*/
 
     /*if (screen.width > screen.height) {
