76bc2b782c9d4910ec3b7809fd2de40b4721aece
francesco-ftk <francesco.fantechi3@stud.unifi.it>
2021-07-04 08:40:19 +0200

    javascript dinamically added

--- js/map_plugin_init.js
+++ js/map_plugin_init.js
@@ -1,5 +1,28 @@
 $(document).ready(function() {
 	console.log("DOM LOADED");
-     jQuery("#mapid").createMap({serverURL : "server/actions.php"});
-     //jQuery(this).getGallery({serverURL : "server/actions.php"});
+
+	// carico dinamicamente il javascript per l'editing delle foto
+
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        // Codice da eseguire nel caso di un dispositivo mobile
+        console.log("mobile")
+        loadjsfile("js/drawingMobile.js", "js");
+    } else {
+        // Codice da eseguire nel caso di un dispositivo tradizionale
+        console.log("desktop")
+        loadjsfile("js/drawingDesktop.js", "js");
+    }
+
+    jQuery("#mapid").createMap({serverURL : "server/actions.php"});
+    //jQuery(this).getGallery({serverURL : "server/actions.php"});
 });
+
+function loadjsfile(filename, filetype){
+    if (filetype=="js"){ //if filename is a external JavaScript file
+        var fileref=document.createElement('script')
+        fileref.setAttribute("type","text/javascript")
+        fileref.setAttribute("src", filename)
+    }
+    if (typeof fileref!="undefined")
+        document.getElementsByTagName("head")[0].appendChild(fileref)
+}
