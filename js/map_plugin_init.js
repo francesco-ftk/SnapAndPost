a524b6361c110c9be5f5ca344a99ac162784eca2
francesco-ftk <francesco.fantechi3@stud.unifi.it>
2021-07-24 09:58:41 +0200

    resize gallery #1

--- js/map_plugin_init.js
+++ js/map_plugin_init.js
@@ -9,6 +9,19 @@ $(document).ready(function () {
     }
 
     jQuery("#mapid").createMap({serverURL: "server/actions.php"});
+
+    jQuery(window).on('resize', function(){
+        $slider=$('#slider');
+        $coverSlider = $('#coverSlider');
+        $container6 = $('#container6');
+        if($coverSlider.css("display")=='flex'){
+            $container6.css("width", "100vw");
+            $slider.parent().css({
+                width: $slider.width(),
+            });
+        }
+    });
+
 });
 
 function loadjsfile(filename, filetype) {
